acbc5bfd161a2ad97b6a97c4ca51bcc2087ce31f
Umutgumuss <umtgms0666@gmail.com>
2024-01-07 19:12:01 +0300

    Form uygulaması
    Gerekli olan formlar oluşturuldu. Odeme,UyeGoruntuleme,GuncelleSil,UyeEkle,Anasayfa ve giris kısımları eklendi. Eksiklikler tamamlanacaktır.

---
+++ umutotomasyon/giris.cs
@@ -0,0 +1,30 @@
+namespace umutotomasyon
+{
+    public partial class giris : Form
+    {
+        public giris()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
